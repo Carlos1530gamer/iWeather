d92cae822bf19687d87989dc022abfe104778bde
Carlos Daniel Hernandez Chauteco <gamercarlos1530@gmail.com>
2019-04-03 10:53:12 -0600

    -add class for usersDefaults

---
+++ iWeather/Modules/Utilities/UserDefaultsMethods/UserDefaultsMethods.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefaultsMethods.swift
+//  iWeather
+//
+//  Created by Carlos Daniel Hernandez Chauteco on 4/3/19.
+//  Copyright © 2019 chauteco. All rights reserved.
+//
+
+import Foundation
+
+class UserDefaultsMethods {
+    private let deafults = UserDefaults.standard
+}
